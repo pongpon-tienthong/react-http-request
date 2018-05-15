ba12d45bc518c8cde5a0ef5ab8a8e0ac7f5375ad
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-05-15 10:25:03 -0500

    make a post selectable

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './FullPost.css';
+import "./FullPost.css";
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
-        );
-        return post;
+  render() {
+    let post = (
+      <div className="FullPost">
+        <p style={{ textAlign: "center" }}>Please select a Post!</p>
+      </div>
+    );
+
+    if (this.props.id) {
+      post = (
+        <div className="FullPost">
+          <h1>Title</h1>
+          <p>Content</p>
+          <div className="Edit">
+            <button className="Delete">Delete</button>
+          </div>
+        </div>
+      );
     }
+
+    return post;
+  }
 }
 
 export default FullPost;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,25 +7,44 @@ import "./Blog.css";
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   };
 
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
-      this.setState({ posts: res.data });
+      const posts = res.data.splice(0, 6).map(post => {
+        return {
+          ...post,
+          author: "David"
+        };
+      });
+
+      this.setState({ posts: posts });
     });
   }
 
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
+
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post key={post.id} title={post.title} />;
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
     });
 
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
