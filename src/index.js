3f59a35a6567ffe3f4a37cce7826184e20b3f137
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-05-15 12:21:41 -0500

    add global interceptor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+// interceptor for requests
+axios.interceptors.request.use(
+  request => {
+    console.log(request);
+    return request;
+  },
+  err => {
+    console.error(err);
+    return Promise.reject(err);
+  }
+);
+
+// interceptor for responses
+axios.interceptors.response.use(
+  res => {
+    console.log(res);
+    return res;
+  },
+  err => {
+    console.error(err);
+    return Promise.reject(err);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
