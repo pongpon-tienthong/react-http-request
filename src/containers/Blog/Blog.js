29c4eaa6687b94da58d83b8866bf24e17d0e9c9f
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-05-26 18:35:14 -0500

    Setup dynamic router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 import FullPost from "./FullPost/FullPost";
 import "./Blog.css";
 
@@ -26,8 +26,10 @@ class Blog extends Component {
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Route path="/" exact component={Posts} />
-        <Route path="/new-post" component={NewPost} />
-        <Route path="/:id" exact component={FullPost} />
+        <Switch>
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/posts/:id" exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
